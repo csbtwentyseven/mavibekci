e6f57a1bd3bde67a4b5cdee35126498e0f42fb7d
unknown <csaidberk@gmail.com>
2020-01-05 00:08:09 +0300

    Butonlara clicklistener eklendi.Kontrol okları eklendi.Far ve Otomatik pilot butonları eklendi.

--- app/src/main/java/com/cesebe/tubitak2020/MainActivity.java
+++ app/src/main/java/com/cesebe/tubitak2020/MainActivity.java
@@ -3,6 +3,9 @@ package com.cesebe.tubitak2020;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
@@ -14,13 +17,100 @@ import com.google.firebase.database.ValueEventListener;
 public class MainActivity extends AppCompatActivity {
 
 
+    public void oklistener(){
+        Button yukari = findViewById(R.id.yukari_ok);
+        Button sag = findViewById(R.id.sag_ok);
+        Button sol = findViewById(R.id.sol_ok);
+        Button far = findViewById(R.id.far_ok);
+        Button oto = findViewById(R.id.oto_ok);
+        final TextView textView = findViewById(R.id.db);
+
+        yukari.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if(event.getAction() == MotionEvent.ACTION_UP){
+
+                    textView.setText("YUKARI");
+                    return true;
+                }
+                else{
+                    textView.setText("YUKARI DEĞİL");
+                }
+                return false;
+            }
+        });
+
+        sag.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if(event.getAction() == MotionEvent.ACTION_UP){
+
+                    textView.setText("SAG");
+                    return true;
+                }
+                else{
+                    textView.setText("SAG DEĞİL");
+                }
+                return false;
+            }
+        });
+
+        sol.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if(event.getAction() == MotionEvent.ACTION_UP){
+
+                    textView.setText("SOL");
+                    return true;
+                }
+                else{
+                    textView.setText("SOL DEĞİL");
+                }
+                return false;
+            }
+        });
+
+        far.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if(event.getAction() == MotionEvent.ACTION_UP){
+
+                    textView.setText("FAR");
+                    return true;
+                }
+                else{
+                    textView.setText("FAR DEĞİL");
+                }
+                return false;
+            }
+        });
+
+        oto.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if(event.getAction() == MotionEvent.ACTION_UP){
+
+                    textView.setText("OTO");
+                    return true;
+                }
+                else{
+                    textView.setText("OTO DEĞİL");
+                }
+                return false;
+            }
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final TextView textView = findViewById(R.id.dbtest);
+        final TextView textView = findViewById(R.id.db);
+        oklistener();
+
+
+
 
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("message");
