e9b53be86ab39320f28062295518862047042026
unknown <csaidberk@gmail.com>
2020-01-13 17:05:00 +0300

    arduino motor far ve mesafe sensörü kodları eklendi-test edilmedi.

--- mavibekciraspberrypi/arducom/arducom.ino
+++ mavibekciraspberrypi/arducom/arducom.ino
@@ -1,17 +1,29 @@
 //Cezeri Robotics 2020 Tubitak
 //Mavi Bekçi
 
-int kul_veri = 0;
+int kul_veri = 100;
 const int in1 = 11;
 const int in2 = 10;
-const int in3 = 9;
-const int in4 = 8;
+const int in3 = 5;
+const int in4 = 6;
+const int led = 3;
 
+int trigPin = 6;
+int echoPin = 7;
+
+long sure;
+long uzaklik;
 void setup() {
+  Serial.begin(9600); /* Seri haberlesme baslatildi */
   pinMode(in1, OUTPUT);
   pinMode(in2, OUTPUT);
   pinMode(in3, OUTPUT);
   pinMode(in4, OUTPUT);
+  pinMode(led, OUTPUT);
+
+  pinMode(trigPin, OUTPUT); /* trig pini çıkış olarak ayarlandı */
+  pinMode(echoPin, INPUT); /* echo pini giriş olarak ayarlandı */
+
 
 }
 
@@ -54,28 +66,60 @@ void loop() {
 
 }
 
-void dur(){
- 
+void dur() {
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, LOW);
 }
 
-void ileri(){
-  
+void ileri() {
+  digitalWrite(in1, HIGH);//sağdaki motor ileri
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, HIGH);//soldaki motor ileri
+  digitalWrite(in4, LOW);
 }
 
-void sag(){
+void sag() {
+  digitalWrite(in1, HIGH);//sağdaki motor ileri
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, LOW);
 
-  
 }
 
-void sol(){
+void sol() {
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, HIGH);//soldaki motor ileri
+  digitalWrite(in4, LOW);
 
- 
 }
 
-void oto(){
+void oto() {
+  digitalWrite(trigPin, LOW); /* sensör pasif hale getirildi */
+  delayMicroseconds(5);
+  digitalWrite(trigPin, HIGH); /* Sensore ses dalgasının üretmesi için emir verildi */
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);  /* Yeni dalgaların üretilmemesi için trig pini LOW konumuna getirildi */
+  sure = pulseIn(echoPin, HIGH); /* ses dalgasının geri dönmesi için geçen sure ölçülüyor */
+  uzaklik = sure / 29.1 / 2; /* ölçülen sure uzaklığa çevriliyor */
 
-  
+  if (uzaklik <= 5)
+  {
+    digitalWrite(in1, LOW);
+    digitalWrite(in2, LOW);
+    digitalWrite(in3, HIGH);//soldaki motor ileri
+    digitalWrite(in4, LOW);
+  }
+  Serial.println(uzaklik);
+
 }
-void far(){
-  
+void far() {
+  if (digitalRead(led)) { // led yanıksa söndür,sönükse yak.
+    digitalWrite(led, LOW);
+  }
+  else {
+    digitalWrite(led, HIGH);
+  }
 }
